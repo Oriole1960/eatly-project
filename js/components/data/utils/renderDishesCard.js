45e21665603b84d854fe8bfa080c672591f46638
Сергей <rovnins@gmail.com>
2025-08-21 16:19:24 +0300

    Добавил анимации для отрендеренных карт товаров и ресторанов. Добавил новую секцию на стартовую страницу.

--- js/components/data/utils/renderDishesCard.js
+++ js/components/data/utils/renderDishesCard.js
@@ -4,65 +4,80 @@ const products = [
         time: "24min",
         rating: 4.8,
         price: "12.99",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Healthy"
     },
     {
         title: "Swe Dish",
         time: "34min",
         rating: 4.9,
         price: "19.99",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Trending"
     },
     {
         title: "Vegan Salad",
         time: "15min",
         rating: 4.7,
         price: "9.25",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Supreme"
     },
     {
         title: "Chicken Air",
         time: "26min",
         rating: 4.6,
         price: "12.99",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Healthy"
     },
     {
         title: "Pig Hell",
         time: "22min",
         rating: 4.9,
         price: "10.99",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Supreme"
     },
     {
         title: "Fish Smile",
         time: "24min",
         rating: 4.7,
         price: "19.99",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Trending"
     },
     {
         title: "Super Chicken",
         time: "30min",
         rating: 4.8,
         price: "13.99",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Healthy"
     },
     {
         title: "Cow Deluxe",
         time: "45min",
         rating: 5.0,
-        price: "45.99",
-        image: "./assets/images/forCompanents/card__chiken-hell.png"
+        image: "./assets/images/forCompanents/card__chiken-hell.png",
+        category: "Trending"
     },
 ];
 
+// Справочник
+const categoriesMap = {
+    Healthy: {text: "Healthy", class: "category--healthy"},
+    Trending: {text: "Trending", class: "category--trending"},
+    Supreme: {text: "Supreme", class: "category--supreme"},
+};
 
-    const template = document.getElementById("dish-card-template");
-    const container = document.querySelector(".dishes__card--conteiner");
+const template = document.getElementById("dish-card-template");
+const container = document.querySelector(".dishes__card--conteiner");
 
+//  Ограничение на загрузку в grid 5-ти карточек
 const OneRowProduct = products.slice(0, 5)
 
+// Рендер карточек
 OneRowProduct.forEach((product) => {
     const card = template.content.cloneNode(true);
 
@@ -70,8 +85,17 @@ OneRowProduct.forEach((product) => {
     const img = card.querySelector(".dishes__card--img img");
     img.src = product.image;
     img.alt = product.title;
-    // название
 
+    // Категория
+
+    const categoryConteiner = card.querySelector(".category--card--dishes");
+    const category = categoriesMap[product.category];
+    if (category) {
+        categoryConteiner.textContent = category.text;
+        categoryConteiner.classList.add(category.class);
+    }
+
+    // название
     const title = card.querySelector(".dishes__card--text > p");
     title.textContent = product.title;
     // время и рейтинг
@@ -90,5 +114,4 @@ OneRowProduct.forEach((product) => {
     `;
 
     container.append(card);
-
 })
